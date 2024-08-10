0084d34696d4ccefca840153899a708bf2af0dd7
eniamza <eniamza0@gmail.com>
2024-08-10 15:58:32 +0600

    Feat: Init and Translation boilerplate code

---
+++ commands/main/translate.js
@@ -0,0 +1,33 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+const transformMissingCharacters = (name) => {
+    return name
+        .replace(/g/g, 'c')
+        .replace(/w/g, 'v')
+        .replace(/z/g, 's')
+        .replace(/k/g, 'c')
+        .replace(/q/g, 'cu')
+        .replace(/y/g, 'j');
+};
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('translate')
+		.setDescription('Translate English to Caion!')
+        .addStringOption(option =>
+            option.setName('input')
+                .setDescription('Enter a sentence or a word in English')
+                .setRequired(true)),
+	async execute(interaction) {
+        const input = interaction.options.getString('input');
+
+        //Filter out all the punctuation marks and special characters and convert the input to lowercase
+
+        const filteredInput = input.toLowerCase().replace(/[^a-zA-Z0-9]/g, '');
+        const finalInput = transformMissingCharacters(filteredInput);
+        console.log(finalInput);
+
+
+		await interaction.reply(`Your input is: ${finalInput}`);
+	},
+};
